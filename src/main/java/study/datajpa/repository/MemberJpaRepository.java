fb4c48dc1bee5d1d6c441db1ebee557b9e958703
redHeadSet <stikfascube@gmail.com>
2021-11-17 17:12:15 +0900

    Member 및 Team 관련 함수 추가

--- src/main/java/study/datajpa/repository/MemberJpaRepository.java
+++ src/main/java/study/datajpa/repository/MemberJpaRepository.java
@@ -5,18 +5,39 @@ import org.springframework.stereotype.Repository;
 import study.datajpa.entity.Member;
 
 import javax.persistence.EntityManager;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 @RequiredArgsConstructor
 public class MemberJpaRepository {
     private final EntityManager em;
 
-    public Long save(Member member){
+    public Long save(Member member) {
         em.persist(member);
         return member.getId();
     }
 
+    public void delete(Member member) {
+        em.remove(member);
+    }
+
     public Member findOne(Long id) {
         return em.find(Member.class, id);
+    }
+
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+
+    public Optional<Member> findById(Long id) {
+        Member member = em.find(Member.class, id);
+        return Optional.ofNullable(member);
+    }
+
+    public Long count(){
+        return em.createQuery("select count(m) from Member m", Long.class)
+                .getSingleResult();
     }
 }
--- src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -1,20 +1,23 @@
 package study.datajpa.repository;
 
 import org.assertj.core.api.Assertions;
+import org.assertj.core.api.Fail;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.entity.Member;
 
+import java.util.List;
+
 @SpringBootTest
 @Transactional
 class MemberJpaRepositoryTest {
     @Autowired
     MemberJpaRepository memberJpaRepository;
-    
+
     @Test // jupiter api 사용할 것
-    public void 회원가입(){
+    public void test1() {
         // given
         Member member = new Member("test1");
         Long savedId = memberJpaRepository.save(member);
@@ -24,5 +27,46 @@ class MemberJpaRepositoryTest {
 
         // then
         Assertions.assertThat(member).isEqualTo(findMember);
+    }
+
+    @Test
+    public void test2() {
+        // given
+        Member member1 = new Member("mem1", 10);
+        Member member2 = new Member("mem2", 20);
+        memberJpaRepository.save(member1);
+        memberJpaRepository.save(member2);
+
+        // when
+        List<Member> all = memberJpaRepository.findAll();
+        Long count = memberJpaRepository.count();
+
+        // then
+        Assertions.assertThat(count).isEqualTo(2);
+
+        all.forEach(each -> {
+            if((member1.getId() != each.getId()) && (member2.getId() != each.getId()))
+                Assertions.fail("id Fail");
+            else
+                System.out.println("each run : " + each.getUsername());
+        });
+
+
+        // given
+
+        // when
+        memberJpaRepository.delete(member1);
+
+        // then
+        List<Member> all2 = memberJpaRepository.findAll();
+        Long count2 = memberJpaRepository.count();
+
+        Assertions.assertThat(1L).isEqualTo(count2);
+        all2.forEach(each -> {
+            if(each.getId() != member2.getId())
+                Assertions.fail("delete Fail");
+            else
+                System.out.println("member2 : " + member2.getUsername());
+        });
     }
 }
