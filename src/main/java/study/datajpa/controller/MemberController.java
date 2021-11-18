24fcea535408743f1d4f5743262fe018ec1af67b
redHeadSet <stikfascube@gmail.com>
2021-11-18 18:39:12 +0900

    web에서 page 처리

--- src/main/java/study/datajpa/controller/MemberController.java
+++ src/main/java/study/datajpa/controller/MemberController.java
@@ -1,22 +1,30 @@
 package study.datajpa.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 import study.datajpa.entity.Member;
 import study.datajpa.repository.MemberRepository;
 
 import javax.annotation.PostConstruct;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 public class MemberController {
     private final MemberRepository memberRepository;
 
     @PostConstruct
     public void init() {
-        memberRepository.save(new Member("test1"));
+        memberRepository.save(new Member("mem1", 10));
+        memberRepository.save(new Member("mem2", 20));
+        memberRepository.save(new Member("mem3", 30));
+        memberRepository.save(new Member("mem4", 40));
+        memberRepository.save(new Member("mem5", 50));
+        memberRepository.save(new Member("mem6", 60));
     }
 
     // 기존 id를 받아서 조회하는 방식
@@ -34,4 +42,25 @@ public class MemberController {
     public String findById2(@PathVariable("id") Member member) {
         return member.getUsername();
     }
+
+    // web에서도 Pageable 지원
+    // localhost:8080/members?page=0 으로 입력하면 기본적으로 size 값이 20으로 들어감
+    // page=0
+    // size=3
+    // sort=id,desc (여러 번 입력도 가능)
+    // global 설정은 application.yml 파일에서 설정 가능
+    @GetMapping("/members")
+    public Page<Member> list(Pageable pageable) {
+        return memberRepository.findAll(pageable);
+    }
+
+    // 각 조회 상태에 따라 @PageableDefault 값에서 개별 처리 가능
+    @GetMapping("/members/custom")
+    public Page<Member> listCustom(@PageableDefault(size = 5, sort = "username") Pageable pageable) {
+        return memberRepository.findAll(pageable);
+    }
+
+    // Page 처리 추가 고려 사항
+    // 1. DTO를 사용하여 처리할 것 - page.map 처리
+    // 2. page index는 0부터 처리하는 걸로 하는 걸로 처리한다 - page index 화면에서는 +1해서 보여주는 걸로
 }
